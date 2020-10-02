1a7edc70b6025aa84dd1e89660d3ea7aaa5ea1c6
PPrasai <letters.prasai@gmail.com>
2020-10-02 13:39:08 +0545

    (chore) write FormGroup for Form-A Step-5

--- src/app/pages/forms/form-a/form-a-step-five/form-a-step-five.component.ts
+++ src/app/pages/forms/form-a/form-a-step-five/form-a-step-five.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslationServiceEn } from '../../../../services/i18n/translation-gen.service';
-
-
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'cov-form-a-step-five',
@@ -9,6 +8,19 @@ import { TranslationServiceEn } from '../../../../services/i18n/translation-gen.
   styleUrls: ['./form-a-step-five.component.scss']
 })
 export class FormAStepFiveComponent implements OnInit {
+
+  form = new FormGroup({
+    $key: new FormControl(null),
+    rbHospitalizedOrIsolated: new FormControl(''),
+    ifYesHospitalizationDate: new FormControl(''),
+    ifYesHospiralName: new FormControl(''),
+
+    rbIsolationLocation: new FormControl(''),
+    isolationLocation: new FormControl(''),
+    isolatedDate: new FormControl(''),
+
+    rbPatientInVentilator: new FormControl('')
+  });
 
   constructor(public t: TranslationServiceEn) { }
 
