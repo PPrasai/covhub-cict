8366237c5d95f02cf68ae3d4a13ad67fd56e1ff9
PPrasai <letters.prasai@gmail.com>
2020-10-02 14:57:41 +0545

    (chore) write FormGroup for Form-A Step-7

--- src/app/pages/forms/form-a/form-a-step-seven/form-a-step-seven.component.ts
+++ src/app/pages/forms/form-a/form-a-step-seven/form-a-step-seven.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslationServiceEn } from '../../../../services/i18n/translation-gen.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'cov-form-a-step-seven',
@@ -8,6 +9,46 @@ import { TranslationServiceEn } from '../../../../services/i18n/translation-gen.
 })
 export class FormAStepSevenComponent implements OnInit {
 
+  form = new FormGroup({
+    $key: new FormControl(null),
+    rbCaseHasFamily: new FormControl(''),
+    rbCaseServedByAnyone: new FormControl(''),
+    ifYesNumberOfMembers: new FormControl(null),
+
+    rbCaseServedByHealthWorker: new FormControl(''),
+
+    rbCaseUsedPublicTransport: new FormControl(''),
+    rbIfYesWhatMedium: new FormControl(''),
+    ifYesWhen: new FormControl(''),
+
+    route: new FormControl(''),
+    vehicleNumber: new FormControl(''),
+    seatNumber: new FormControl(''),
+    taxiNumber: new FormControl(''),
+    taxiEmbarkLocation: new FormControl(''),
+    flightNumber: new FormControl(''),
+    flightSeatNumber: new FormControl(''),
+    travelOrigin: new FormControl(''),
+    travelDestination: new FormControl(''),
+
+    rbSchoolOrOffice: new FormControl(''),
+    ifYesAddress: new FormControl(''),
+    ifYesPhone: new FormControl(''),
+    authorityName: new FormControl(''),
+
+    rbInCloseProximity: new FormControl(''),
+
+    rbSocialContact: new FormControl(''),
+
+    rbCaseAttendProgram: new FormControl(''),
+    programAddress: new FormControl(''),
+    programPhone: new FormControl(''),
+    programDate: new FormControl(''),
+
+    helpfulInfoForContactTracing: new FormControl(''),
+
+    rbOneOrMoreContactInPartSix: new FormControl('')
+  })
 
   constructor(public t: TranslationServiceEn) { }
 
