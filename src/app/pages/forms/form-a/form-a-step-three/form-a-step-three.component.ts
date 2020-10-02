54522bc3f041d4bf9ed0a97dce17ce678eff56ea
PPrasai <letters.prasai@gmail.com>
2020-10-02 13:13:00 +0545

    (chore) write FormGroup for Form-A Step-3

--- src/app/pages/forms/form-a/form-a-step-three/form-a-step-three.component.ts
+++ src/app/pages/forms/form-a/form-a-step-three/form-a-step-three.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { TranslationServiceEn } from '../../../../services/i18n/translation-gen.service';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'cov-form-a-step-three',
@@ -11,6 +11,32 @@ export class FormAStepThreeComponent implements OnInit {
 
   constructor(public t: TranslationServiceEn) { }
 
+  form = new FormGroup({
+    $key: new FormControl(null),
+    rbNasopharyngeal: new FormControl(''),
+    sampleCollectionDate: new FormControl(''),
+    sampleDispatchDate: new FormControl(''),
+    labTestResultDate: new FormControl(''),
+    labTestResult: new FormControl(''),
+
+    rbOropharyngeal: new FormControl(''),
+    oropharyngealSampleCollectionDate: new FormControl(''),
+    oropharyngealSampleDispatchDate: new FormControl(''),
+    oropharyngealLabTestResultDate: new FormControl(''),
+    oropharyngealLabTestResult: new FormControl(''),
+
+    rbOther: new FormControl(''),
+    otherSampleCollectionDate: new FormControl(''),
+    otherSampleDispatchDate: new FormControl(''),
+    otherLabTestResultDate: new FormControl(''),
+    otherLabTestResult: new FormControl(''),
+    otherTestSpecification: new FormControl(''),
+
+    rbLabType: new FormControl(''),
+    rbAnyOtherTestLab: new FormControl(''),
+    ifYesTestName: new FormControl(''),
+    ifYesTestResult: new FormControl('')
+  });
   ngOnInit(): void {
   }
 
