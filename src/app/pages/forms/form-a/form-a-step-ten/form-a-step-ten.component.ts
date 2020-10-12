660d6c3e4b15da136eb8d5418ec252fa52098013
PPrasai <letters.prasai@gmail.com>
2020-10-12 16:49:30 +0545

    complete form A last part

--- src/app/pages/forms/form-a/form-a-step-ten/form-a-step-ten.component.ts
+++ src/app/pages/forms/form-a/form-a-step-ten/form-a-step-ten.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { TranslationServiceEn } from '../../../../services/i18n/translation-gen.service';
+import { FormAService } from '../form-a.service';
 
 @Component({
   selector: 'cov-form-a-step-ten',
@@ -8,13 +10,36 @@ import { TranslationServiceEn } from '../../../../services/i18n/translation-gen.
 })
 export class FormAStepTenComponent implements OnInit {
 
-  constructor(public t: TranslationServiceEn) { }
+  form = new FormGroup({
+    reportResendDate: new FormControl(''),
+    rbPreDischargeOrDeathSymptomatic: new FormControl(''),
+    ifYesSymptomDate: new FormControl(''),
+    rbHospitalized: new FormControl(''),
+    ifHospitalizedDate: new FormControl(''),
+    rbCaseInICU: new FormControl(''),
+    rbCaseInVentilation: new FormControl(''),
+    rbCaseHaveMembraneOxy: new FormControl(''),
+
+    rbHealthResult: new FormControl(''),
+    ifOtherLetKnow: new FormControl(''),
+
+    dischargeDate: new FormControl(''),
+    ifDischargedLastTestDate: new FormControl(''),
+    rbLabTestResult: new FormControl('')
+  });
+
+  constructor(
+    public t: TranslationServiceEn,
+    public formAService: FormAService
+  ) {
+    this.formAService.newFormFlag$.subscribe(flag => {
+      if (flag) {
+        console.log('STEP LAST: prepare data instruction received');
+        this.formAService.aggregateFormData(this.form);
+      }
+    })
+  }
 
   ngOnInit(): void {
   }
-
-  newFormAData() {
-    console.log('new form a step 10');
-  }
-
 }
